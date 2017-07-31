79f09df9654bf85f7edf1b8ae65bf157f170a672
Zhao Wang <zhao@platoteam.com>
2017-07-31 12:13:22 -0700

    testing single compiled libgmp.a

--- TestFactorize/ViewController.swift
+++ TestFactorize/ViewController.swift
@@ -23,23 +23,27 @@ class ViewController: UIViewController {
     }
 
     func testPrimeFactorize() {
-        measureBackflushAndVerify(number: "1051473895475943185188722643")
-        measureBackflushAndVerify(number: "1093208757760918931821458961")
-        measureBackflushAndVerify(number: "244461137395190299921769851")
-        measureBackflushAndVerify(number: "226314805358035834565826361")
-        measureBackflushAndVerify(number: "203349049209916202464236661")
-        measureBackflushAndVerify(number: "255172064656116717706908667")
-        measureBackflushAndVerify(number: "277102727591499977422018369")
-        measureBackflushAndVerify(number: "184942209274967876075214379")
-        measureBackflushAndVerify(number: "237831436598635880570130907")
-        measureBackflushAndVerify(number: "261405767112385155541030303")
-        measureBackflushAndVerify(number: "276936463777187195972531707")
+//        measureBackflushAndVerify(number: "1051473895475943185188722643")
+//        measureBackflushAndVerify(number: "1093208757760918931821458961")
+//        measureBackflushAndVerify(number: "244461137395190299921769851")
+//        measureBackflushAndVerify(number: "226314805358035834565826361")
+//        measureBackflushAndVerify(number: "203349049209916202464236661")
+//        measureBackflushAndVerify(number: "255172064656116717706908667")
+//        measureBackflushAndVerify(number: "277102727591499977422018369")
+//        measureBackflushAndVerify(number: "184942209274967876075214379")
+//        measureBackflushAndVerify(number: "237831436598635880570130907")
+//        measureBackflushAndVerify(number: "261405767112385155541030303")
+//        measureBackflushAndVerify(number: "276936463777187195972531707")
         measureBackflushAndVerify(number: "247771065637429073788327459")
     }
 
     fileprivate func measureBackflushAndVerify(number: String) {
         let n = GMPInteger(number)
-        print(n)
+        print(n.description)
+        let factors = n.calculateFactors(debug: true)
+        for f in factors {
+            print(GMPInteger(f).description)
+        }
     }
 }
 
