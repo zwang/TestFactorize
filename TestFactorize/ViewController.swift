a7a28453631f755d94a5028ee3b17f4b72e9a2d7
Zhao Wang <zhao@platoteam.com>
2017-08-10 12:55:18 -0700

    works if uncheck "include bitcode" option when uploading app

--- TestFactorize/ViewController.swift
+++ TestFactorize/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var ss: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        testPrimeFactorize()
+        ss = testPrimeFactorize()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let v = UITextView()
+        self.view.addSubview(v)
+        v.frame = self.view.frame
+        v.frame.origin = CGPoint(x: 5, y: 10)
+        v.text = ss.joined(separator: "\n")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,24 +31,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func testPrimeFactorize() {
-        measureBackflushAndVerify(number: "1051473895475943185188722643")
-        measureBackflushAndVerify(number: "1093208757760918931821458961")
-        measureBackflushAndVerify(number: "244461137395190299921769851")
-        measureBackflushAndVerify(number: "226314805358035834565826361")
-        measureBackflushAndVerify(number: "203349049209916202464236661")
-        measureBackflushAndVerify(number: "255172064656116717706908667")
-        measureBackflushAndVerify(number: "277102727591499977422018369")
-        measureBackflushAndVerify(number: "184942209274967876075214379")
-        measureBackflushAndVerify(number: "237831436598635880570130907")
-        measureBackflushAndVerify(number: "261405767112385155541030303")
-        measureBackflushAndVerify(number: "276936463777187195972531707")
-        measureBackflushAndVerify(number: "247771065637429073788327459")
+    func testPrimeFactorize() -> [String] {
+        var ss = [String]()
+        let start = CACurrentMediaTime()
+        ss.append(measureBackflushAndVerify(number: "1051473895475943185188722643"))
+        ss.append(measureBackflushAndVerify(number: "1093208757760918931821458961"))
+        ss.append(measureBackflushAndVerify(number: "244461137395190299921769851"))
+        ss.append(measureBackflushAndVerify(number: "226314805358035834565826361"))
+        ss.append(measureBackflushAndVerify(number: "203349049209916202464236661"))
+        ss.append(measureBackflushAndVerify(number: "255172064656116717706908667"))
+        ss.append(measureBackflushAndVerify(number: "277102727591499977422018369"))
+        ss.append(measureBackflushAndVerify(number: "184942209274967876075214379"))
+        ss.append(measureBackflushAndVerify(number: "237831436598635880570130907"))
+        ss.append(measureBackflushAndVerify(number: "261405767112385155541030303"))
+        ss.append(measureBackflushAndVerify(number: "276936463777187195972531707"))
+        ss.append(measureBackflushAndVerify(number: "247771065637429073788327459"))
+        let stop = CACurrentMediaTime()
+        ss.append("Total time: \(stop - start) seconds.")
+        return ss
     }
 
-    fileprivate func measureBackflushAndVerify(number: String) {
+    fileprivate func measureBackflushAndVerify(number: String) -> String {
         let n = GMPInteger(number).calculateFactors(debug: false)
-        print(n.map({ GMPInteger($0).description }))
+        return number + " = \(n.map({ GMPInteger($0).description }))"
     }
 }
 
